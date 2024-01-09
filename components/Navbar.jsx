7e4c8e3fe94dcf82be54c7fad2ef9256cb58023c
muzamil khan <muzamilfootballer111111@gmail.com>
2024-01-08 19:09:24 -0800

    i commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+     <Link className="text-white font-bold" href={"/"}>Muzamil </Link>
+     <Link className="bg-white p-2 rounded-md" href={"/addTopic"}>
+        Add Topic
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ components/TopicsList.jsx
@@ -0,0 +1,58 @@
+'use client'
+import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
+import { useEffect } from "react";
+
+
+    
+const getTopics = async () => {
+  try {
+    useEffect(() => {
+        getTopics()
+    }, []);
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+
+  }
+};
+
+
+
+
+export default async function TopicsList() {
+    
+  const { topics } = await getTopics();
+       
+  return (
+    <>
+      {topics.sort((a, b) => a.itemM > b.itemM ? 1 : -1).map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
+
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
